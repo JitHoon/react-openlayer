76bae6e4e544377903eccfe71831702527f36c29
jithoon <imessi0336@gmail.com>
2024-06-13 13:36:08 +0900

    feat: Popup Text

--- app/components/Guide/PopupText.tsx
+++ app/components/Guide/PopupText.tsx
@@ -3,18 +3,17 @@
 import { CustomMarker, InnerText, MapContainer, icon } from "react-openlayers7";
 import styles from "../../page.module.css";
 
-icon.marker = "/images/marker-basic.png";
-icon.selected = "/images/marker-selected.png";
+const PopupText = () => {
+  // InnerText의 isPopup 속성을 사용하면 text를 숨길 수 있습니다.
 
-const TextWithMarker = () => {
   return (
     <MapContainer center={[126.840884, 35.190816]} width="30%" height="30vh">
-      <h2 className={styles.title}>Text with Marker</h2>
+      <h2 className={styles.title}>Popup Text</h2>
       <CustomMarker center={[126.840884, 35.190816]}>
-        <InnerText>Marker</InnerText>
+        <InnerText isPopup>Marker</InnerText>
       </CustomMarker>
     </MapContainer>
   );
 };
 
-export default TextWithMarker;
+export default PopupText;
--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,14 @@ import "react-openlayers7/dist/index.css";
 
 import IntroMap from "./components/Setup/IntroMap";
 import TextWithMarker from "./components/Guide/TextWithMarker";
+import PopupText from "./components/Guide/PopupText";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <IntroMap />
       <TextWithMarker />
+      <PopupText />
     </main>
   );
 }
